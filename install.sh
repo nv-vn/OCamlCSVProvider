844646cbe9a8657dbca639c158842eb0df76d970
Leonid Rozenberg <leonidr@gmail.com>
2016-01-13 19:00:44 -0500

    Link META to _oasis

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -n `ocamlfind query ppx_csv_provider` ]; then
-  echo 'Removing previously installed versions of `ppx_csv_provider`'
-  ocamlfind remove ppx_csv_provider
-fi
-ocamlfind install ppx_csv_provider ./src/META
-ocamlfind install -add ppx_csv_provider ./csv_provider.native
---
+++ src/ppx_csv_provider.ml
@@ -0,0 +1,3 @@
+open Ast_mapper
+
+let _ = register "csv_provider" Csv_provider.csv_mapper
